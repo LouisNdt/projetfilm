306eacfcf50249b4b771ba969bf00f988732510b
vins <vrizzetto@btslivh.eu>
2020-09-08 16:09:08 +0200

    nveau test

--- cinema/e2e/src/app.e2e-spec.ts
+++ cinema/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to MoviesApp!');
+    expect(page.getTitleText()).toEqual('cinema app is running!');
   });
 
   afterEach(async () => {
