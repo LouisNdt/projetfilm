8087b768c1b0f8baeb745c20de5018defdd448aa
lnardot <louis.ndt31@gmail.com>
2020-09-09 16:03:46 +0200

    Container and navbar component

--- cinema/src/app/app.module.ts
+++ cinema/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
